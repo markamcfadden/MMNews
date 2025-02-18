88abd18d64e6abf0a000c2eeae81729410fff1be
markamcfadden <mcfadden.a.mark@gmail.com>
2025-02-18 13:36:06 +0000

    add navbar from bootstrap

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,23 @@
-function Navbar() {
+import { Link } from "react-router-dom";
+import { Container, Nav, Navbar, Button } from "react-bootstrap";
+
+function NavBar() {
   return (
-    <nav className="navbar">
-      <h1>TartanTalk</h1>
-      <button>Login</button>
-    </nav>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand>Tartan Talk</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+          </Nav>
+          <Button variant="secondary">Login</Button>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
-export default Navbar;
+export default NavBar;
