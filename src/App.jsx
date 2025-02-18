ed6f83beeec32b2b1e817ba09035df7246fecd25
markamcfadden <mcfadden.a.mark@gmail.com>
2025-02-18 16:17:51 +0000

    add Context to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import { Route, Routes } from "react-router-dom";
+import { UseProvider } from "./UserContext";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Homepage from "./pages/Homepage";
 import ArticlePage from "./pages/ArticlePage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Homepage />}></Route>
-        <Route path="/:article_id" element={<ArticlePage />}></Route>
-      </Routes>
+      <UseProvider>
+        <Routes>
+          <Route path="/" element={<Homepage />}></Route>
+          <Route path="/:article_id" element={<ArticlePage />}></Route>
+          <Route path="/login" element={<LoginPage />}></Route>
+        </Routes>
+      </UseProvider>
     </div>
   );
 }
