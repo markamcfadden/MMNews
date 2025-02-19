a2e10f3107eea3e92a8e296bc91b1db53e274890
markamcfadden <mcfadden.a.mark@gmail.com>
2025-02-19 14:33:49 +0000

    add trashcan component for deleting comments

--- src/components/TrashCan.jsx
+++ src/components/TrashCan.jsx
@@ -4,11 +4,12 @@ import { UserContext } from "../UserContext";
 import { useContext, useState, useEffect } from "react";
 import { deleteComment } from "../api";
 
-function TrashCan({ author, comment_id }) {
+function TrashCan({ author, comment_id, setComments }) {
   const { loggedInUser } = useContext(UserContext);
   const [canDelete, setCanDelete] = useState(false);
   const [error, setError] = useState("");
   const [successMessage, setSuccessMessage] = useState("");
+  const [isDeleting, setIsDeleting] = useState(false);
 
   useEffect(() => {
     if (loggedInUser) {
@@ -21,27 +22,31 @@ function TrashCan({ author, comment_id }) {
   function handleDelete(e) {
     e.preventDefault();
     setError("");
+    setIsDeleting(true);
     deleteComment(comment_id)
       .then(() => {
         setSuccessMessage("Comment deleted successfully");
+        setIsDeleting(false);
+        setComments((prev) =>
+          prev.filter((comment) => comment.comment_id !== comment_id)
+        );
         setTimeout(() => setSuccessMessage(""), 3000);
       })
       .catch(() => {
         setError("Failed to delete comment, please try again");
+        setIsDeleting(false);
       });
   }
 
   return canDelete ? (
-    <Button variant="secondary" onClick={handleDelete}>
-      <Trash />
-    </Button>
+    <div>
+      <Button variant="secondary" onClick={handleDelete} disabled={isDeleting}>
+        <Trash />
+      </Button>
+      {error ? <p>{error}</p> : null}{" "}
+      {successMessage ? <p>{successMessage}</p> : null}
+    </div>
   ) : null;
-  {
-    error ? <p>{error}</p> : null;
-  }
-  {
-    successMessage ? <p>{successMessage}</p> : null;
-  }
 }
 
 export default TrashCan;
