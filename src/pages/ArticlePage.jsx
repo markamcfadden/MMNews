3cde7a1d5ec8bf7cad6575fcecdef11f9e4e38af
markamcfadden <mcfadden.a.mark@gmail.com>
2025-02-19 11:15:37 +0000

    add AddComment component to articles page

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import NavBar from "../components/Navbar";
 import SelectedArticle from "../components/Selected-Article";
 import CommentsList from "../components/CommentsList";
+import AddComment from "../components/AddComment";
 
 function ArticlePage() {
   const { article_id } = useParams();
@@ -10,6 +11,7 @@ function ArticlePage() {
     <div>
       <NavBar />
       <SelectedArticle article_id={article_id} />
+      <AddComment article_id={article_id} />
       <CommentsList article_id={article_id} />
     </div>
   );
