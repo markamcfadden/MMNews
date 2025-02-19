a356c1b31835964638f7d6778b1307b5c6dea796
markamcfadden <mcfadden.a.mark@gmail.com>
2025-02-19 13:23:35 +0000

    add local storage to save user info

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -1,9 +1,19 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const UserContext = createContext();
 
 export const UseProvider = ({ children }) => {
-  const [loggedInUser, setLoggedInUser] = useState(null);
+  const [loggedInUser, setLoggedInUser] = useState(() => {
+    return JSON.parse(localStorage.getItem("user")) || null;
+  });
+
+  useEffect(() => {
+    if (loggedInUser) {
+      localStorage.setItem("user", JSON.stringify(loggedInUser));
+    } else {
+      localStorage.removeItem("user");
+    }
+  }, [loggedInUser]);
 
   return (
     <UserContext.Provider value={{ loggedInUser, setLoggedInUser }}>
