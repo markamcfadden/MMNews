d7d6214e4ac210a52d1a593e2d78c44f64e0377f
markamcfadden <mcfadden.a.mark@gmail.com>
2025-02-20 10:27:22 +0000

    add url params

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,13 +1,16 @@
 import ArticlesList from "../components/Article-list";
 import NavBar from "../components/Navbar";
 import Searchbar from "../components/Searchbar";
+import { useParams } from "react-router-dom";
 
 function Homepage() {
+  const { selectedTopic } = useParams();
+
   return (
     <div>
       <NavBar />
       <Searchbar />
-      <ArticlesList />
+      <ArticlesList topic={selectedTopic} />
     </div>
   );
 }
