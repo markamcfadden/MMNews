da3ffa93499f9d75dd702730a34d4c2294c2df14
markamcfadden <mcfadden.a.mark@gmail.com>
2025-02-23 21:22:49 +0000

    add AddArticlePage

---
+++ src/pages/AddArticlePage.jsx
@@ -0,0 +1,13 @@
+import Footer from "../components/Footer";
+import NavBar from "../components/Navbar";
+
+function AddArticlePage() {
+  return (
+    <div>
+      <NavBar />
+      <Footer />
+    </div>
+  );
+}
+
+export default AddArticlePage;
