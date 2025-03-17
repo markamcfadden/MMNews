35ec6eab7f02a12b640c0432c80e447ca2fad738
markamcfadden <mcfadden.a.mark@gmail.com>
2025-03-17 14:54:19 +0000

    add community page

---
+++ src/pages/CommunityPage.jsx
@@ -0,0 +1,13 @@
+import Footer from "../components/Footer";
+import NavBar from "../components/Navbar";
+
+function CommunityPage() {
+  return (
+    <>
+      <NavBar></NavBar>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default CommunityPage;
