c1d31c6137ff0a1ab033f974c1c61e1224a1650f
markamcfadden <mcfadden.a.mark@gmail.com>
2025-03-17 15:40:57 +0000

    add trash can component to article card

--- src/components/Article-card.jsx
+++ src/components/Article-card.jsx
@@ -2,6 +2,9 @@ import { Link } from "react-router-dom";
 import Card from "react-bootstrap/Card";
 import LikeButton from "./LikeButton";
 import { FaComment } from "react-icons/fa";
+import { useContext } from "react";
+import { UserContext } from "../UserContext";
+import ArticleTrashCan from "./ArticleTrash";
 
 function ArticleCard({
   article_id,
@@ -13,6 +16,8 @@ function ArticleCard({
   comment_count,
   body,
 }) {
+  const { loggedInUser } = useContext(UserContext);
+
   return (
     <Link to={`/article/${article_id}`} className="article-card-link">
       <Card className="article-card">
@@ -20,6 +25,11 @@ function ArticleCard({
         <div className="article-title">
           <Card.Title>{title}</Card.Title>
         </div>
+        {loggedInUser?.username === author && (
+          <div>
+            <ArticleTrashCan author={author} article_id={article_id} />
+          </div>
+        )}
         <Card.Body>
           <Card.Text className="article-body">{body ? body : null}</Card.Text>
           <div className="article-meta">
