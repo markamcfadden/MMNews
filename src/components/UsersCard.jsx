36a6e9d89c75c005a570406466b673391d125321
markamcfadden <mcfadden.a.mark@gmail.com>
2025-03-17 16:33:00 +0000

    add new components

---
+++ src/components/UsersCard.jsx
@@ -0,0 +1,45 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+function UserCard({ username, avatar_url }) {
+  return (
+    <Card to={`/${username}`}>
+      <Avatar src={avatar_url} alt={`${username}'s avatar`} />
+      <Username>@{username}</Username>
+    </Card>
+  );
+}
+
+export default UserCard;
+
+const Card = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-decoration: none;
+  background: white;
+  padding: 16px;
+  border-radius: 12px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  width: 150px;
+  transition: transform 0.2s;
+
+  &:hover {
+    transform: scale(1.05);
+  }
+`;
+
+const Avatar = styled.img`
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+  object-fit: cover;
+  aspect-ratio: 1/1;
+  margin-bottom: 10px;
+`;
+
+const Username = styled.p`
+  font-size: 1rem;
+  font-weight: bold;
+  color: #333;
+`;
---
+++ src/components/UsersList.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import { fetchAllUsers } from "../api";
+import styled from "styled-components";
+import UserCard from "./UsersCard";
+
+function UsersList() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetchAllUsers()
+      .then((usersFromApi) => {
+        setUsers(usersFromApi);
+      })
+      .catch((err) => {
+        console.log(err, "error fetching users");
+      });
+  }, []);
+
+  return (
+    <UsersContainer>
+      {users.map((user) => (
+        <UserCard
+          key={user.username}
+          username={user.username}
+          avatar_url={user.avatar_url}
+        />
+      ))}
+    </UsersContainer>
+  );
+}
+
+const UsersContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 20px;
+  justify-content: center;
+  padding: 20px;
+  max-width: 900px;
+  margin: auto;
+
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+`;
+
+export default UsersList;
