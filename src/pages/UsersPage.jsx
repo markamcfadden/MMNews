dbfe2957b4676c1c865341bdd920923f6b7bf3b3
markamcfadden <mcfadden.a.mark@gmail.com>
2025-03-17 16:32:20 +0000

    add user card to users page

--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -1,10 +1,27 @@
 import Footer from "../components/Footer";
 import NavBar from "../components/Navbar";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { fetchUserByUsername } from "../api";
+import UserCard from "../components/UsersCard";
+import UsersArticles from "../components/UsersArticles";
 
 function UsersPage() {
+  const { username } = useParams();
+  console.log(username);
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    fetchUserByUsername(username).then((userFromApi) => {
+      setUser(userFromApi);
+    });
+  }, []);
+
   return (
     <div>
       <NavBar />
+      <UserCard username={user.username} avatar_url={user.avatar_url} />
+      <UsersArticles username={user.username} />
       <Footer />
     </div>
   );
