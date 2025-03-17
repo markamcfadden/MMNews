42b2cc2dbda8555fbe48849c75a87f9a57050d15
markamcfadden <mcfadden.a.mark@gmail.com>
2025-03-17 16:31:59 +0000

    add users cards to page

--- src/pages/CommunityPage.jsx
+++ src/pages/CommunityPage.jsx
@@ -1,10 +1,12 @@
 import Footer from "../components/Footer";
 import NavBar from "../components/Navbar";
+import UsersList from "../components/UsersList";
 
 function CommunityPage() {
   return (
     <>
       <NavBar></NavBar>
+      <UsersList></UsersList>
       <Footer></Footer>
     </>
   );
