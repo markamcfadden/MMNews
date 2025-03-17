ba7b5f86c7bf9d3ecbd7a0150fee33313265652c
markamcfadden <mcfadden.a.mark@gmail.com>
2025-03-17 14:55:22 +0000

    add form to page

--- src/pages/AddArticlePage.jsx
+++ src/pages/AddArticlePage.jsx
@@ -1,10 +1,12 @@
 import Footer from "../components/Footer";
+import AddForm from "../components/AddForm";
 import NavBar from "../components/Navbar";
 
 function AddArticlePage() {
   return (
     <div>
       <NavBar />
+      <AddForm />
       <Footer />
     </div>
   );
